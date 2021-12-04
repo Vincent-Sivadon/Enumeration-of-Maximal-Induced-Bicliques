39e77d0580e22167d611453b17946108fb62e1b2
Chaima Elharti <elharti-chaima@hotmail.fr>
2021-12-04 17:05:21 +0100

    Debugging independantSets function and implementing printSet

--- maximalIndependantSets.c
+++ maximalIndependantSets.c
@@ -1,31 +1,32 @@
 #include "adjlst.h"
 
-
-
-typedef struct {
+typedef struct
+{
   size_t *vertices;
   size_t size;
   size_t reserved_size;
 } dyn_array;
 
-
-void init_dynarray(dyn_array* a, size_t size) {
+void init_dynarray(dyn_array *a, size_t size)
+{
   if (!a)
     exit(1); // Erreur
   a->vertices = malloc(sizeof(size_t) * size);
   a->reserved_size = size;
-}
+};
 
-void free_dynarray(dyn_array* a, size_t size) {
+void free_dynarray(dyn_array *a, size_t size)
+{
   if (!a)
     exit(1); // Erreur
   free(a->vertices);
   a->vertices = NULL;
   a->reserved_size = 0;
   a->size = 0;
-}
+};
 
-void append_dynarray(dyn_array* a, int elem) {
+void append_dynarray(dyn_array *a, int elem)
+{
   if (!a)
     exit(1); // Erreur
   if (a->size >= a->reserved_size)
@@ -34,46 +35,44 @@ void append_dynarray(dyn_array* a, int elem) {
     a->reserved_size = (a->reserved_size + 1) * 2;
   }
   a->vertices[a->size] = elem;
-}
+  a->size++;
+};
 
-
-
-
-dyn_array independantSets(Graph* graph){
-    int N = sizeof(graph);
-    int v = sizeof(N * sizeof(Vertex*)); // sizeof adjlst
-    dyn_array set; 
-    init_dynarray(&set, 1);
-    dyn_array* set_ptr = malloc(sizeof(dyn_array) * N);
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j<v;j++){
-            if (!checkEdge(graph, i,j)){
-                for (size_t k = 0; k < N; k++) {
-                    init_dynarray(&set_ptr[i], v); 
-                }
-            }
+dyn_array *independantSets(Graph *graph)
+{
+  int N = sizeof(graph);
+  int v = sizeof(N * sizeof(Vertex *)); // sizeof adjlst
+  dyn_array set;
+  init_dynarray(&set, 1);
+  dyn_array *set_ptr = malloc(sizeof(dyn_array) * v);
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < v; j++)
+    {
+      if (checkEdge(graph, i, j)==1)
+      {
+        for (size_t k = 0; k < N; k++)
+        {
+          append_dynarray(&set_ptr[k], k);
         }
+      }
     }
-    return set;
+  }
+  return set_ptr;
+};
 
-}
+void printSet(dyn_array *set)
+{
+  for (int v = 0; v < set->size; v++)
+  {
+    printf("%ld \t", set->vertices[v]);
+  }
+};
 
-
-
-int main(){
-    Graph* graph = genRandGraph(10);
-    independantSets(graph);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main(int argc, char **argv)
+{
+  Graph *graph = genRandGraph(10);
+  printGraph(graph);
+  dyn_array *set = independantSets(graph);
+  printSet(set);
+};
